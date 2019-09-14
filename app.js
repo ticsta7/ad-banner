6d2f72b73458a54a83bbadcdf4421288eb49cc77
ticsta7 <ticsta7@163.com>
2019-09-14 12:29:52 +0800

    坑

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-$(function() 
+
+$(document).ready(function()
 {
+	// 为什么直接控制台可以，但是放到插件里面就不行，是页面没渲染完？？
+	
+
+	$("#v6_pl_content_publishertop .W_input").innerText='qqqq';
+
+
+
+	 $('em.spac1').innerText='写点什么';
+	 $('em.spac2').innerText='吧';
+	 $('em.spac3').innerText='总有人会听';
+	 $('em.spac4').innerText='。';
+	
+	
   /*
   console.log("loaded");
 
