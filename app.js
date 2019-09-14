84e08def3f780053421375cd56908563242e7548
ticsta7 <ticsta7@163.com>
2019-09-14 20:56:20 +0800

    v1.1

--- app.js
+++ app.js
@@ -6,5 +6,9 @@
   $('em.spac3').text('总有人会听');
   $('em.spac4').text('。');
 
-	 
+
+//下面2条要在页面渲染之前执行，才能生效
+var hearderImg=$('.headpic .W_face_radius').attributes.src.value;  //获取头像地址
+$('.gn_name .W_ficon').innerHTML=(` < img class='W_face_radius' src='https://tvax4.sinaimg.cn/crop.0.0.1080.1080.180/006DTvpjly8g5a8c61g0dj30u00u0juj.jpg?KID=imgbed,tva&Expires=1568465335&ssig=NEl8NIzom3' width='26' height='26'>`)   //替换头像icon
+	
 	
