148af63626cb54a8cc815e1eac453aa9d9dde4d3
ticsta7 <ticsta7@163.com>
2019-09-14 15:58:23 +0800

    v1.0

--- app.js
+++ app.js
@@ -8,18 +8,3 @@
 
 	 
 	
-	
-  /*
-  console.log("loaded");
-
-  console.log($("#v6_pl_rightmod_attfeed"));
-  $("#v6_pl_rightmod_attfeed").remove();
-
-  setTimeout( ()=> 
-  {
-  	$("#v6_pl_rightmod_attfeed").remove();
-  	console.log("找不熬~");
-  } , 2000 );
-
-  */
-  
